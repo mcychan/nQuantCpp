6842cf773121f829ffbb06e5b47cd6a939d162c6
Miller Cy Chan <miller.chan@hotmail.com>
2018-02-12 08:06:10 +0800

    Initial commit

---
+++ Resource.h
@@ -0,0 +1,17 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by nQuantCpp.rc
+//
+
+#define IDS_APP_TITLE			103
+
+// 下一個新增物件的預設值
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE	101
+#define _APS_NEXT_COMMAND_VALUE		40001
+#define _APS_NEXT_CONTROL_VALUE		1000
+#define _APS_NEXT_SYMED_VALUE		101
+#endif
+#endif
