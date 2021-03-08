da3b657dbeeb1712a2761979abf1d7ae0a32dcdf
Miller Cy Chan <miller.chan@gmail.com>
2021-03-08 10:46:58 +0800

    Add files via upload

--- nQuantCpp/PnnLABQuantizer.cpp
+++ nQuantCpp/PnnLABQuantizer.cpp
@@ -155,7 +155,7 @@ namespace PnnLABQuant
 
 		int h, l, l2;
 		if (quan_sqrt && nMaxColors < 64)
-			ratio = min(1.0, proportional - pow(nMaxColors, 2.52) / pixelMap.size());
+			ratio = min(1.0, proportional - nMaxColors * exp(4.202) / pixelMap.size());
 		else if (quan_sqrt)
 			ratio = min(1.0, pow(nMaxColors, 1.05) / pixelMap.size());
 		else
