030688a9489ef17dfe0edffa106f2d9de5106a1b
Miller Cy Chan <miller.chan@gmail.com>
2021-03-20 11:43:09 +0800

    Add files via upload

--- nQuantCpp/PnnLABQuantizer.cpp
+++ nQuantCpp/PnnLABQuantizer.cpp
@@ -146,17 +146,13 @@ namespace PnnLABQuant
 			quan_rt = 0;
 
 		if (quan_rt > 0)
-			bins[0].cnt = (int)_sqrt(bins[0].cnt);
-		else if (quan_rt < 0)
-			bins[0].cnt = (int)cbrt(bins[0].cnt);
+			bins[0].cnt = (int)_sqrt(bins[0].cnt);		
 		for (int i = 0; i < maxbins - 1; ++i) {
 			bins[i].fw = i + 1;
 			bins[i + 1].bk = i;
 
 			if (quan_rt > 0)
 				bins[i + 1].cnt = (int)_sqrt(bins[i + 1].cnt);
-			else if (quan_rt < 0)
-				bins[i + 1].cnt = (int)cbrt(bins[i + 1].cnt);
 		}
 
 		int h, l, l2;
