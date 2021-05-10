b0d67d9ead508dfac4c3341aaeec9cdae5e8dbb2
Miller Cy Chan <miller.chan@gmail.com>
2021-05-10 12:55:58 +0800

    Add files via upload

--- nQuantCpp/EdgeAwareSQuantizer.cpp
+++ nQuantCpp/EdgeAwareSQuantizer.cpp
@@ -25,7 +25,7 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #include "stdafx.h"
 #include "EdgeAwareSQuantizer.h"
-#include "DivQuantizer.h"
+#include "MedianCut.h"
 #include "bitmapUtilities.h"
 #include "CIELABConvertor.h"
 
@@ -613,8 +613,8 @@ namespace EdgeAwareSQuant
 		if (nMaxColors == 256 && pDest->GetPixelFormat() != PixelFormat8bppIndexed)
 			pDest->ConvertFormat(PixelFormat8bppIndexed, DitherTypeSolid, PaletteTypeCustom, pPalette, 0);
 
-		DivQuant::DivQuantizer divQuantizer;
-		divQuantizer.quant_varpart_fast(pixels.data(), pixels.size(), pPalette);
+		MedianCutQuant::MedianCut mmcQuantizer;
+		mmcQuantizer.quantizeImg(pixels, bitmapWidth, saliencyMap, pPalette, nMaxColors);
 
 		const float divisor = hasSemiTransparency ? 255.0f : 1.0f;
 		vector<vector_fixed<float, 4> > palette(nMaxColors);
