965c452a03c21cd6d22b39707dee3fcef6d1bae0
Miller Cy Chan <miller.chan@gmail.com>
2021-09-02 15:05:06 +0800

    Update HilbertCurve.cpp

--- nQuantCpp/HilbertCurve.cpp
+++ nQuantCpp/HilbertCurve.cpp
@@ -73,20 +73,16 @@ namespace Riemersma
 
 	        errorq.erase(errorq.begin());
 	        Color c2 = m_pPalette->Entries[m_qPixels[x + y * m_width]];
-	        error[0] = r_pix > BYTE_MAX ? BYTE_MAX : r_pix - c2.GetR();
-	        error[1] = g_pix > BYTE_MAX ? BYTE_MAX : g_pix - c2.GetG();
-	        error[2] = b_pix > BYTE_MAX ? BYTE_MAX : b_pix - c2.GetB();
-	        error[3] = a_pix > BYTE_MAX ? BYTE_MAX : a_pix - c2.GetA();
+	        error[0] = r_pix - c2.GetR();
+	        error[1] = g_pix - c2.GetG();
+	        error[2] = b_pix - c2.GetB();
+	        error[3] = a_pix - c2.GetA();
 	        
 	        for(int j = 0; j < sizeof(error.p) / sizeof(float); ++j) {
                 if (abs(error[j]) < DITHER_MAX)
                     continue;
 
-                error[j] -= error[j] < 0 ? -DITHER_MAX : DITHER_MAX;
-                if (abs(error[j]) < DITHER_MAX)
-                    continue;
-
-	        	error[j] = error[j] < 0 ? -DITHER_MAX + 1 : DITHER_MAX - 1;
+                error[j] = 0;
 	        }
 	        errorq.emplace_back(error);
 	    }
