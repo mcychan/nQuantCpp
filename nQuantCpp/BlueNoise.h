5abe938dd0c0687cba4b53a1276e69a6913cbdc2
Miller Cy Chan <miller.chan@gmail.com>
2022-08-16 09:58:49 +0800

    Update BlueNoise.h

--- nQuantCpp/BlueNoise.h
+++ nQuantCpp/BlueNoise.h
@@ -3,5 +3,6 @@
 
 namespace BlueNoise
 {
+	extern const char* RAW_BLUE_NOISE;
 	void dither(const UINT width, const UINT height, const ARGB* pixels, const ColorPalette* pPalette, DitherFn ditherFn, GetColorIndexFn getColorIndexFn, unsigned short* qPixels, const float weight = 1.0f);
 }
