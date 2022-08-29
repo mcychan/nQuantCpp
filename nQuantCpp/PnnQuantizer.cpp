67e0f7a9ac324ecb6394095a453ca78b74941dc4
Miller Cy Chan <miller.chan@gmail.com>
2022-08-29 09:48:01 +0800

    Update PnnQuantizer.cpp

--- nQuantCpp/PnnQuantizer.cpp
+++ nQuantCpp/PnnQuantizer.cpp
@@ -47,7 +47,7 @@ namespace PnnQuant
 		auto wb = bin1.bc;
 
 		int start = 0;
-		if (BlueNoise::RAW_BLUE_NOISE[idx & 4095] > -77)
+		if (BlueNoise::RAW_BLUE_NOISE[idx & 4095] > -88)
 			start = 1;
 
 		for (int i = bin1.fw; i; i = bins[i].fw) {
