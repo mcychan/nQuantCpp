6dc5f612b36f7e9ee07215e4322c8ce23cd66558
Miller Cy Chan <miller.chan@gmail.com>
2023-04-26 20:06:42 +0800

    Update GilbertCurve.cpp

--- nQuantCpp/GilbertCurve.cpp
+++ nQuantCpp/GilbertCurve.cpp
@@ -105,7 +105,7 @@ namespace Peano
 		auto yDiff = diffuse ? 1 : CIELABConvertor::Y_Diff(c1, c2);
 
 		int errLength = denoise ? error.length() - 1 : 0;
-		auto ditherMax = (m_hasAlpha || DITHER_MAX > 9) ? (BYTE) sqr(_sqrt(DITHER_MAX) + 1) : DITHER_MAX;
+		auto ditherMax = (m_hasAlpha || DITHER_MAX > 9) ? (BYTE) sqr(_sqrt(DITHER_MAX) + 2) : DITHER_MAX;
 
 		for (int j = 0; j < errLength; ++j) {
 			if (abs(error.p[j]) >= ditherMax) {
