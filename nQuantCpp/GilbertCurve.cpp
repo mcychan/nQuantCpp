0fc5b7c64be5c0ddc0d41516bbe91f6500a1c3a1
Miller Cy Chan <miller.chan@gmail.com>
2023-11-14 20:11:15 +0800

    Add files via upload

--- nQuantCpp/GilbertCurve.cpp
+++ nQuantCpp/GilbertCurve.cpp
@@ -243,7 +243,7 @@ namespace Peano
 		ditherMax = (hasAlpha || DITHER_MAX > 9) ? (BYTE)sqr(_sqrt(DITHER_MAX) + edge) : DITHER_MAX;
 		if (pPalette->Count / weight > 5000 && (weight > .045 || (weight > .01 && pPalette->Count <= 64)))
 			ditherMax = (BYTE)sqr(5 + edge);
-		else if (pPalette->Count / weight < 3200 && pPalette->Count > 16 && pPalette->Count < 256)
+		else if (pPalette->Count / weight < 3200 && pPalette->Count >= 16 && pPalette->Count < 256)
 			ditherMax = (BYTE)sqr(5 + edge);
 		thresold = DITHER_MAX > 9 ? -112 : -64;
 		auto pLookup = make_unique<short[]>(USHRT_MAX + 1);
