489f2ae0a1f90f73327887287fd5c0d92ac1e43f
Miller Cy Chan <miller.chan@gmail.com>
2025-02-19 12:27:55 +0800

    Update GilbertCurve.cpp

--- nQuantCpp/GilbertCurve.cpp
+++ nQuantCpp/GilbertCurve.cpp
@@ -129,7 +129,7 @@ namespace Peano
 				c2 = BlueNoise::diffuse(pixel, m_pPalette[qPixelIndex], beta * .5f / m_saliencies[bidx], strength, x, y);
 				if (CIELABConvertor::U_Diff(pixel, c2) > (8 * acceptedDiff))
 					c2 = BlueNoise::diffuse(pixel, m_pPalette[qPixelIndex], beta / m_saliencies[bidx], strength, x, y);
-				if (m_nMaxColor > 8 && CIELABConvertor::U_Diff(pixel, c2) > (8 * acceptedDiff)) {
+				if (m_nMaxColor <= 8 && CIELABConvertor::U_Diff(pixel, c2) > (8 * acceptedDiff)) {
 					Color c1 = Color::MakeARGB(a_pix, r_pix, g_pix, b_pix);
 					c2 = BlueNoise::diffuse(c1, m_pPalette[qPixelIndex], beta * m_saliencies[bidx], strength, x, y);
 				}
